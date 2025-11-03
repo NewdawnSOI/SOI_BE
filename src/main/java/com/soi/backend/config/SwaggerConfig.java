e12361dcb508e923a2830900787f859d21d746a1
ParkJunhyun0304 <162578133+ParkJunhyun0304@users.noreply.github.com>
2025-11-03 23:41:09 +0900

    chore : swagger 권한 확인 추가

--- src/main/java/com/soi/backend/config/SwaggerConfig.java
+++ src/main/java/com/soi/backend/config/SwaggerConfig.java
@@ -8,6 +8,14 @@ import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.List;
 
@@ -42,5 +50,37 @@ public class SwaggerConfig {
                         .name("SOI")
                         .email("itisnewdawn@gmail.com")
                         .url("https://github.com/NewdawnSOI"));
+    }
+
+    @Configuration
+    @EnableWebSecurity
+    public static class SwaggerSecurityConfig {
+        @Value("${swagger.user}")
+        private String swaggerUser;
+
+        @Value("${swagger.pass}")
+        private String swaggerPassword;
+
+        @Bean
+        public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+            http
+                    .authorizeHttpRequests(auth -> auth
+                            .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").authenticated()
+                            .anyRequest().permitAll()
+                    )
+                    .httpBasic(Customizer.withDefaults())
+                    .csrf(csrf -> csrf.disable());
+            return http.build();
+        }
+
+        @Bean
+        public UserDetailsService users() {
+            UserDetails user = User.builder()
+                    .username(swaggerUser)
+                    .password(swaggerPassword)
+                    .roles("ADMIN")
+                    .build();
+            return new InMemoryUserDetailsManager(user);
+        }
     }
 }
