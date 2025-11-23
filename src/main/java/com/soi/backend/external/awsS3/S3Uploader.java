8b7449051e17236ae89e002a6031d838a73ef5f1
ParkJunhyun0304 <162578133+ParkJunhyun0304@users.noreply.github.com>
2025-11-24 02:12:49 +0900

    chore : swagger 버전 충돌 해결

--- src/main/java/com/soi/backend/external/awsS3/S3Uploader.java
+++ src/main/java/com/soi/backend/external/awsS3/S3Uploader.java
@@ -1,29 +1,33 @@
 package com.soi.backend.external.awsS3;
 
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.net.URL;
-import java.time.LocalDate;
-import java.util.Date;
-import java.util.Optional;
-import java.util.UUID;
-
-import com.amazonaws.HttpMethod;
-import com.amazonaws.services.s3.model.GeneratePresignedUrlRequest;
-import com.soi.backend.domain.media.entity.FileType;
-import com.soi.backend.global.exception.CustomException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import com.amazonaws.services.s3.AmazonS3Client;
-import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.soi.backend.domain.media.entity.FileType;
+import com.soi.backend.global.exception.CustomException;
 
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.core.sync.RequestBody;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.s3.S3Client;
+import software.amazon.awssdk.services.s3.model.PutObjectRequest;
+import software.amazon.awssdk.services.s3.presigner.S3Presigner;
+import software.amazon.awssdk.services.s3.presigner.model.GetObjectPresignRequest;
+import software.amazon.awssdk.services.s3.model.GetObjectRequest;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.time.Duration;
+import java.time.LocalDate;
+import java.util.Optional;
+import java.util.UUID;
 
 @Slf4j
 @RequiredArgsConstructor
@@ -31,58 +35,87 @@ import lombok.extern.slf4j.Slf4j;
 @Service
 public class S3Uploader {
 
-    private final AmazonS3Client amazonS3Client;
+    private final S3Client s3Client;
 
     @Value("${cloud.aws.s3.bucketName}")
     private String bucket;
+    @Value("${cloud.aws.credentials.accessKey}")
+    private String accessKey;
+    @Value("${cloud.aws.credentials.secretKey}")
+    private String secretKey;
 
-    // MultipartFile을 전달받아 File로 전환한 후 S3에 업로드
+
+    /**
+     * MultipartFile → Temp File → S3 업로드 → Temp 삭제
+     */
     public String upload(MultipartFile multipartFile, FileType fileType, Long id) throws IOException {
         File uploadFile = convert(multipartFile)
                 .orElseThrow(() -> new CustomException("MultipartFile -> File 전환 실패", HttpStatus.INTERNAL_SERVER_ERROR));
 
-        // 파일 경로 생성
-//        String filePath = buildPath(dirName, id, multipartFile.getOriginalFilename());
-
-        // key 발급
         String key = buildKey(fileType.toString(), id, multipartFile.getOriginalFilename());
 
-        // S3 업로드 (private로)
-        amazonS3Client.putObject(new PutObjectRequest(bucket, key, uploadFile));
+        PutObjectRequest request = PutObjectRequest.builder()
+                .bucket(bucket)
+                .key(key)
+                .contentType(multipartFile.getContentType())
+                .build();
 
-        // 로컬 임시 파일 삭제
-        removeNewFile(uploadFile);
+        try {
+            s3Client.putObject(request, RequestBody.fromFile(uploadFile));
+        } catch (Exception e) {
+            throw new CustomException("S3 업로드 실패", HttpStatus.INTERNAL_SERVER_ERROR);
+        } finally {
+            removeNewFile(uploadFile);
+        }
 
         return key;
     }
 
+    /**
+     * Presigned URL 생성 (GET)
+     */
+    public String getPressigneUrl(String key) {
+
+        S3Presigner presigner = S3Presigner.builder()
+                .credentialsProvider(
+                        StaticCredentialsProvider.create(
+                                AwsBasicCredentials.create(
+                                        accessKey,
+                                        secretKey
+                                )
+                        )
+                )
+                .region(Region.AP_NORTHEAST_2)
+                .build();
+
+        GetObjectRequest getObjectRequest = GetObjectRequest.builder()
+                .bucket(bucket)
+                .key(key)
+                .build();
+
+        GetObjectPresignRequest presignRequest = GetObjectPresignRequest.builder()
+                .signatureDuration(Duration.ofHours(1))
+                .getObjectRequest(getObjectRequest)
+                .build();
+
+        return presigner.presignGetObject(presignRequest).url().toString();
+    }
+
+    /** Temp 파일 삭제 */
     private void removeNewFile(File targetFile) {
         if (targetFile.delete()) {
             log.info("파일이 삭제되었습니다.");
         } else {
-            log.info("파일이 삭제되지 못했습니다.");
+            log.info("파일 삭제 실패");
         }
     }
 
-    public String getPressigneUrl(String key) {
-        Date expiration = new Date();
-        expiration.setTime(expiration.getTime() + 1000 * 60 * 60);
-
-        GeneratePresignedUrlRequest request = new GeneratePresignedUrlRequest(bucket, key)
-                .withMethod(HttpMethod.GET)
-                .withExpiration(expiration);
-
-        URL presignedUrl = amazonS3Client.generatePresignedUrl(request);
-        return presignedUrl.toString();
-    }
-
+    /** MultipartFile → File 변환 */
     private Optional<File> convert(MultipartFile file) {
         try {
-            // 안전한 임시 파일 생성
             String safeName = file.getOriginalFilename()
                     .replaceAll("[^a-zA-Z0-9.\\-_]", "_");
 
-            // /tmp 디렉토리는 EC2와 Docker 환경에서도 항상 쓰기 가능
             File convertFile = File.createTempFile("upload-", "_" + safeName);
 
             try (FileOutputStream fos = new FileOutputStream(convertFile)) {
@@ -92,18 +125,20 @@ public class S3Uploader {
             return Optional.of(convertFile);
 
         } catch (IOException e) {
-            e.printStackTrace();
             return Optional.empty();
         }
     }
 
-    // key 생성 규칙: {yyyy/MM/dd}/user_{id}/{type}_{uuid}_{filename}
+    /**
+     * Key 생성 규칙:
+     * {yyyy/MM/dd}/user_{id}/{category}_{uuid}_{filename}
+     */
     private String buildKey(String category, Long id, String originalFilename) {
         LocalDate now = LocalDate.now();
         String datePath = String.format("%04d/%02d/%02d", now.getYear(), now.getMonthValue(), now.getDayOfMonth());
         String uuid = UUID.randomUUID().toString();
         String safeName = originalFilename == null ? "" : originalFilename.replaceAll("[^a-zA-Z0-9.\\-_]", "_");
+
         return String.format("%s/user_%d/%s_%s_%s", datePath, id, category, uuid, safeName);
     }
-
 }
