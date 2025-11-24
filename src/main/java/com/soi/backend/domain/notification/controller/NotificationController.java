3b0006431045318f833dd58a157160c4e2e8af2c
ParkJunhyun0304 <162578133+ParkJunhyun0304@users.noreply.github.com>
2025-11-24 20:16:36 +0900

    Debug : 카테고리 생성 에러 해결
    카테고리 생성시, 서로가 다 유저일때, 카테고리 - 유저 테이블에 생성하지 않고 알림을 추가하려고 해서 에러가 났었음
    카테고리-유저 테이블에 우선 추가하고, 알림 보내도록 수정

--- src/main/java/com/soi/backend/domain/notification/controller/NotificationController.java
+++ src/main/java/com/soi/backend/domain/notification/controller/NotificationController.java
@@ -1,4 +1,19 @@
 package com.soi.backend.domain.notification.controller;
 
+import com.soi.backend.domain.notification.service.NotificationService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/notification")
+
+@Tag(name = "notification API", description = "알림 관련 API")
 public class NotificationController {
+
+    private final NotificationService notificationService;
+
 }
