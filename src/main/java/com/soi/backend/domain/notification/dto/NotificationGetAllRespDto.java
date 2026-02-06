441264d5e3d47371f096fd333e25f2a5247aded5
ParkJunhyun0304 <162578133+ParkJunhyun0304@users.noreply.github.com>
2026-02-06 16:15:55 +0900

    hotfix : 에러 수정

--- src/main/java/com/soi/backend/domain/notification/dto/NotificationGetAllRespDto.java
+++ src/main/java/com/soi/backend/domain/notification/dto/NotificationGetAllRespDto.java
@@ -2,13 +2,11 @@ package com.soi.backend.domain.notification.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 
 import java.util.List;
 
 @Getter
 @AllArgsConstructor
-@NoArgsConstructor
 
 public class NotificationGetAllRespDto {
     private final Integer friendReqCount; // 친구요청온거 갯수
